b2c98be0c01a7f338a034af1e51b97e0b0bacb8b
Shaykh102 <dignity000@gmail.com>
2025-05-31 19:24:28 +0500

    Refactor Cypress tests: update login tests to use fixture data, clean up conversation tests, and remove unused files.

--- cypress/support/pages/Conversations.js
+++ cypress/support/pages/Conversations.js
@@ -1,1 +1,7 @@
-//methods for dashboard
+export class Conversation_page_tests {
+    WorkspacePageOpen() {
+        cy.url({ timeout: 10000 })
+            .should('include', '/inteligems')
+            .should('be.visible')
+    }
+}
