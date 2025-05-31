536a22915e4a319b67b03fff65cd714dabab75ce
Shaykh102 <dignity000@gmail.com>
2025-05-31 22:23:09 +0500

    Refactor Conversations tests: remove commented-out code, enhance test assertions for default conversation, and clean up structure.

--- cypress/e2e/conversations.cy.js
+++ cypress/e2e/conversations.cy.js
@@ -1,5 +1,5 @@
-/*import { LoginPage } from '../support/pages/LoginPage';
-import { Conversation_page_tests } from '../support/pages/Conversations'; */
+
+
 
 describe('Conversations Tests', () => {
     beforeEach(() => {
@@ -9,28 +9,29 @@ describe('Conversations Tests', () => {
     });
 
     it('Default Conversation open', () => {
-        // Your test code here
-        // The login is already handled in beforeEach
+        cy.url().should('include', 'inteligems.odysseyai.ai/workspace')
+            .should('be.valid');
+      
     });
 
 });
 
-/*describe('Conversation_page_tests', () => {
-   const loginPage = new LoginPage();
-    const conv = new Conversation_page_tests; 
-    const baseURL = "https://app.odysseyai.ai";
-    const email = "shamil@inteligems.io";
-    const password = "*****";
 
-    it('Successfull login', () => {
-        loginPage.visit(baseURL);
-        loginPage.fillEmail(email);
-        loginPage.fillPassword(password);
-        loginPage.submit();
-        loginPage.checkUrlAfterLogin();
-        loginPage.selectTeam();
+ 
+ 
+ 
+ 
+ 
 
-    });
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+ 
 
     it('Opening Conversation page', () => {
         conv.WorkspacePageOpen();
