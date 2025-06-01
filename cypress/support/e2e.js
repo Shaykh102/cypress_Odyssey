a35e6de3486ab23717be1de39cab3f18220a0f48
Shaykh102 <dignity000@gmail.com>
2025-06-01 13:02:25 +0500

    Refactor Cypress test configurations: remove hardcoded user data setup from GitHub Actions and update GITHUB_TOKEN secret reference.

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,3 +15,31 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+
+/*    - name: Create test configurations
+   run: |
+     echo '{
+       "baseUrl": "https://app.odysseyai.ai",
+       "environment": "${{ secrets.CYPRESS_ENVIRONMENT }}"
+     }' > cypress/fixtures/testData.json
+     
+     echo '{
+       "admin": {
+         "email": "${{ secrets.CYPRESS_ADMIN_EMAIL }}",
+         "password": "${{ secrets.CYPRESS_ADMIN_PASSWORD }}",
+         "userId": "${{ secrets.CYPRESS_ADMIN_USER_ID }}",
+         "apiKey": "${{ secrets.CYPRESS_ADMIN_API_KEY }}"
+       },
+       "regular": {
+         "email": "${{ secrets.CYPRESS_REGULAR_EMAIL }}",
+         "password": "${{ secrets.CYPRESS_REGULAR_PASSWORD }}",
+         "userId": "${{ secrets.CYPRESS_REGULAR_USER_ID }}",
+         "apiKey": "${{ secrets.CYPRESS_REGULAR_API_KEY }}"
+       },
+       "readonly": {
+         "email": "${{ secrets.CYPRESS_READONLY_EMAIL }}",
+         "password": "${{ secrets.CYPRESS_READONLY_PASSWORD }}",
+         "userId": "${{ secrets.CYPRESS_READONLY_USER_ID }}",
+         "apiKey": "${{ secrets.CYPRESS_READONLY_API_KEY }}"
+       }
+     }' > cypress/fixtures/users.json */
